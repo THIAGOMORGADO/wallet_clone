d03c1757b22c0646499f442e7f1dc605a243df96
thiago morgado <thiago_morgado@hotmail.com>
2024-11-09 01:33:33 -0300

    feat: finalizando config do styled

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,12 @@ import {
 } from "@expo-google-fonts/poppins";
 import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular_Italic } from "@expo-google-fonts/dm-serif-display";
-import { ThemeProvider } from 'styled-components/native'
 
+import { ThemeProvider } from "styled-components/native";
 import * as SplashScreen from "expo-splash-screen";
-import theme from "./styles/theme";
 
-
+import theme from "../src/styles/theme";
+import { StatusBar } from "expo-status-bar";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -26,32 +26,26 @@ export default function App() {
         Poppins_700Bold,
         DMSans_400Regular,
         DMSerifDisplay_400Regular_Italic,
-    })
+    });
 
     useEffect(() => {
         SplashScreen.hideAsync();
-    }, [fontsloaded])
+    }, [fontsloaded]);
 
-    if(!fontsloaded) {
+    if (!fontsloaded) {
         return null; // or some other loading indicator
     }
 
-
     return (
         <ThemeProvider theme={theme}>
-            <View style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flex: 1,
-            backgroundColor: theme.COLORS.BACKGROUND,
-        }}>
-            <Text style={{
-                fontFamily: theme.FONTS.POPPINSBOLD,
-                fontSize: 32,
-            }}>Hello, World!</Text>
-        </View>
+            <StatusBar 
+                translucent 
+                style="dark" 
+                backgroundColor="transparent"
+            />
+            <View>
+                <Text>Hello, World!</Text>
+            </View>
         </ThemeProvider>
-        
     );
 }
---
+++ src/screens/index.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+import theme from '../styles/theme';
+
+export const Container = styled.View`
+  background-color: ${theme.COLORS.GRAY1};
+`;
